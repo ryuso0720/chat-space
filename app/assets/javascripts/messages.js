6926891a3cab5d72831cc50d4105c044dcb6da2c
ryuso0720 <ryu.cit0720@gmail.com>
2020-04-01 17:08:53 +0900

    自動更新完了

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -2,11 +2,11 @@ $(function(){
   function buildHTML(message) {
     if (message.content && message.image) {
       var html = `<div class="message" data-message-id = ${message.id}>
-        <div class="upper-info">
-        <div class="upper-info__user-talker">
+        <div class="message__upper-info">
+        <div class="message__upper-info__user-talker">
         ${message.user_name}
         </div>
-        <div class="upper-info__date">
+        <div class="message__upper-info__date">
         ${message.created_at}
         </div>
         </div> 
@@ -18,12 +18,12 @@ $(function(){
         </div>
         </div>`
     } else if (message.content) {
-      var html = `<div class="message">
-        <div class="upper-info">
-        <div class="upper-info__talker">
+      var html = `<div class="message" data-message-id=${message.id}>
+        <div class="message__upper-info">
+        <div class="message__upper-info__talker">
         ${message.user_name}
         </div>
-        <div class="upper-info__date">
+        <div class="message__upper-info__date">
         ${message.created_at}
         </div>
         </div>
@@ -34,12 +34,12 @@ $(function(){
         </div>
         </div>`
     } else if (message.image) {
-      var html = `<div class="message">
-        <div class="upper-info">
-        <div class="upper-info__talker">
+      var html = `<div class="message" data-message-id=${message.id}>
+        <div class="message__upper-info">
+        <div class="message__upper-info__talker">
         ${message.user_name}
         </div>
-        <div class="upper-info__date">
+        <div class="message__upper-info__date">
         ${message.created_at} 
         </div>
         </div>
@@ -50,6 +50,36 @@ $(function(){
     };
     return html;
   }
+
+  //インクリメンタルサーチの記述
+
+  //自動更新
+  
+  var reloadMessages = function() {
+    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function (messages) { //通信成功したら、controllerから受け取ったデータ（messages)を引数にとって以下のことを行う
+      var insertHTML = '';//追加するHTMLの入れ物を作る
+      messages.forEach(function (message) {//配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        insertHTML = buildHTML(message); //メッセージが入ったHTMLを取得
+        $('.messages').append(insertHTML);//メッセージを追加
+      })
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');//最新のメッセージが一番下に表示されようにスクロールする。
+    })
+    .fail(function () {
+      alert('自動更新に失敗しました');//
+    });
+  }
+};
+setInterval(reloadMessages, 5000);//5000ミリ秒ごとにreloadMessagesという関数を実行し自動更新を行う。
+});
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -73,4 +103,3 @@ $(function(){
       alert("メッセージ送信に失敗しました");
   });
   })
-});
